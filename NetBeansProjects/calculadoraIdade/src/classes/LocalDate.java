0838d822d252a1dfa4e9463d45cf44a4c952de61
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2024-04-24 19:12:03 -0300

    feat: calculadoraIdade
    Projeto java swing de calculadoraIdade

---
+++ NetBeansProjects/calculadoraIdade/src/classes/LocalDate.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author Skyfall_jack
+ */
+class LocalDate {
+
+    static LocalDate now() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static LocalDate of(int an, int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    int getYear() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
