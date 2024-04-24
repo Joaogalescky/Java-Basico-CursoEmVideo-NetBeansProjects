d4f924d75d5787ba0788622490803f7b362d954e
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2024-04-24 19:24:39 -0300

    feat: Teste Função 02

---
+++ NetBeansProjects/TesteFuncao02/src/testefuncao02/Operacoes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testefuncao02;
+
+/**
+ *
+ * @author Skyfall_jack
+ */
+public class Operacoes {
+    public static String contador(int a, int b){
+        String s = "";
+        
+        for(int i = 0; i <= b; i++){
+            s += i + " ";
+        }
+        return s;
+    }
+}
