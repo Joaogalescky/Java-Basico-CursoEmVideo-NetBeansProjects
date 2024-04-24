585657ae66218c2ebc0296d3db0c4357f3741acf
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2024-04-24 19:15:14 -0300

    feat: Função 01

---
+++ NetBeansProjects/Funcao01/src/funcao01/Fatorial.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package funcao01;
+
+/**
+ *
+ * @author Skyfall_jack
+ */
+public class Fatorial {
+    
+    private int num = 0;
+    private int fat = 1;
+    private String formula = "";
+    
+    public void setValor(int n){
+        num = n;
+        int f = 1;
+        String s = "";
+        
+        for(int i = n; i > 1; i--){
+            f += i;
+            s += i + " X ";
+        }
+        s += "1 = ";
+        fat = f;
+        formula = s;
+    }
+    
+    public int getFatorial(){
+        return fat;
+    }
+    
+    public String getFormula(){
+        return formula;
+    }
+}
